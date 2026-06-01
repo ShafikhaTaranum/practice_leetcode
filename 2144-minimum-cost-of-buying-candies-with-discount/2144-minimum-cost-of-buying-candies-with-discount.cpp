851b8ee6d81b736ef6309f4aa5b5ae1a114c1585
Shafikha Taranum <shafikhataranum@gmail.com>
2026-06-01 19:13:11 +0530

    Time: 0 ms (100%), Space: 14.2 MB (49.81%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(),cost.end(),greater<int>());
+        int min_cost=0;
+        for(int i=0;i<cost.size();i++){
+            if ((i+1)%3==0){
+                continue;
+            }
+            min_cost+=cost[i];
+        }
+        return min_cost;
+    }
+};
